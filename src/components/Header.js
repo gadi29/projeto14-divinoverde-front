bfe6dc4ed9fc93473c26e886e31a0de33f766fb3
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-06 20:35:37 -0300

    implement header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 
 function Header () {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <h1>DivinoVerde</h1>
-      <p>olá, user</p>
-      <ion-icon name="cart-outline"></ion-icon>
+      <h1 onClick={() => navigate('/home')}>DivinoVerde</h1>
+      <div>
+        <p>olá, user</p>
+        <ion-icon name="chevron-down-outline"></ion-icon>
+        <ion-icon name="cart-outline"></ion-icon>
+      </div>
     </Container>
   );
 }
@@ -18,4 +24,40 @@ const Container = styled.header`
 
   width: 100%;
   height: 65px;
+  padding: 0 15px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    font-family: 'Judson', serif !important;
+    color: #FFFFFF;
+    font-size: 32px;
+    cursor: pointer;
+  }
+
+  div {
+    color: #FFFFFF;
+    display: flex;
+    align-items: center;
+
+    p {
+      font-size: 20px;
+
+      margin-right: 3px;
+    }
+  
+    ion-icon {
+      cursor: pointer;
+    }
+
+    ion-icon:last-of-type {
+    font-size: 30px;
+
+    margin-left: 10px;
+    }
+  }
+
+
 `;
