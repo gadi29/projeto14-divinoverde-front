a2f02780d4a9dce01d370f881996741a91107ff9
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-08 17:59:23 -0300

    implement products visualization and adjusts in header

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Header.js";
 import SignIn from "./SignIn.js";
 import Signup from "./Signup.js";
-import UserContext from "../context/UserContext";
 import Address from "./Address.js";
+import Home from "./Home.js";
 import ProductPage from "./ProductPage.js";
+
+import UserContext from "../context/UserContext";
 
 function App() {
   const [userCreate, setUserCreate] = React.useState();
@@ -19,6 +21,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/address" element={<Address />} />
+          <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductPage />} />
         </Routes>
       </BrowserRouter>
